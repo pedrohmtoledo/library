5600b26aeaaf7c67b8615c9fd2f582fc25da53e2
pedrohmtoledo <toledopedro.hmt@gmail.com>
2024-12-23 15:28:55 +0000

    code of the cards

--- javascript.js
+++ javascript.js
@@ -1,12 +1,21 @@
 const myLibrary = [];
 const container = document.querySelector(".container")
-
+let book1 = new Book("harry potter", "jk rowling", 333, "not read")
+let book2 = new Book("harry potter 2", "jk rowling", 333, "not read")
+addBookToLibrary(book1)
+addBookToLibrary(book2)
 // create a dialog to add a new book
 const dialog = document.querySelector("#add-book-dialog");
 const addBookButton = document.querySelector(".addbook");
 const closeDialogButton = document.querySelector(".close-dialog");
 const submitBookButton = document.querySelector("#submit-book")
 addBookButton.addEventListener("click", () => {
+    let title = document.getElementById("title");
+    let author = document.getElementById("author");
+    let pages = document.getElementById("pages");
+    title.value = "";
+    author.value = "";
+    pages.value = "";
     dialog.show()
 })
 
@@ -18,9 +27,10 @@ submitBookButton.addEventListener("click", function(e) {
     const title = document.getElementById("title").value;
     const author = document.getElementById("author").value;
     const pages = document.getElementById("pages").value;
-    console.log(typeof(title))
-    const book = new Book(title, author, pages, status);
+    console.log(myLibrary)
+    const book = new Book(title, author, pages);
     addBookToLibrary(book);
+    container.innerHTML = ""
     displayBook();
     e.preventDefault();
     dialog.close();
@@ -41,11 +51,7 @@ function Book(title, author, pages, status){
 function addBookToLibrary(book){
     myLibrary.push(book);
 }
-// let book1 = new Book("harry potter", "jk rowling", 333, "not read")
-// let book2 = new Book("harry potter 2", "jk rowling", 333, "not read")
-// addBookToLibrary(book1)
-// addBookToLibrary(book2)
-console.log(myLibrary)
+
 
 // display book in cards. first, create a div with class card, loop through library, add the information of the book into the card, push it to the dom.
 // each card has to have their own id
@@ -58,10 +64,34 @@ function displayBook(){
         const bookTitle = document.createElement("p");
         const bookAuthor = document.createElement("p");
         const bookPages = document.createElement("p");
+        const bookButtonToggle = document.createElement("button");
+        const bookButtonDelete = document.createElement("button");
+        const cardButtons = document.createElement("div");
+
         card.setAttribute("class", "card");
+        cardButtons.setAttribute("class", "card-buttons");
+        bookTitle.setAttribute("class", "book-title");
+        bookAuthor.setAttribute("class", "book-author");
+        bookPages.setAttribute("class", "book-pages");
+        bookButtonToggle.setAttribute("class", "card-button");
+        bookButtonDelete.setAttribute("class", "card-button");
+
         card.setAttribute("id", `book${i}`);
-        card.textContent = myLibrary[i].info();
-        container.appendChild(div);
+        bookButtonDelete.setAttribute("id", "book-delete")
+        bookButtonToggle.setAttribute("id", "book-toggle")
+        bookTitle.textContent = `Title: ${myLibrary[i].title}`;
+        bookAuthor.textContent = `Author: ${myLibrary[i].author}`;
+        bookPages.textContent = `Pages: ${myLibrary[i].pages}`;
+        bookButtonDelete.textContent = "Delete"
+        bookButtonToggle.textContent = "Read"
+
+        cardButtons.appendChild(bookButtonDelete);
+        cardButtons.appendChild(bookButtonToggle);
+        card.appendChild(bookTitle);
+        card.appendChild(bookAuthor);
+        card.appendChild(bookPages);
+        card.appendChild(cardButtons);
+        container.appendChild(card);
     
     }
 }
